dd15f5f05bb314397cb64bba90b319ee0d0dc7ef
Melissa <mrspiegel@me.com>
2024-06-19 11:27:59 -0400

    Testing building block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,20 +1,20 @@
 export default function decorate(block) {
-    // Decorate the quote as a blockquote element
-    const quoteDiv = block.querySelector(":scope > div > div");
-    if (quoteDiv) {
-      const blockquote = document.createElement("blockquote");
-      const quoteText = quoteDiv.innerHTML.trim();
-      if (quoteText) {
-        blockquote.innerHTML = `${quoteText}`;
-        quoteDiv.parentElement.replaceWith(blockquote);
-      }
-    }
-  
-    // Decorate the author as a cite element
-    const authorDiv = block.querySelector(":scope > div > div");
-    if (authorDiv) {
-      const cite = document.createElement("cite");
-      cite.innerHTML = `${authorDiv.innerHTML.trim()}`;
-      authorDiv.parentElement.replaceWith(cite);
+  // Decorate the quote as a blockquote element
+  const quoteDiv = block.querySelector(":scope > div > div");
+  if (quoteDiv) {
+    const blockquote = document.createElement("blockquote");
+    const quoteText = quoteDiv.innerHTML.trim();
+    if (quoteText) {
+      blockquote.innerHTML = `${quoteText}`;
+      quoteDiv.parentElement.replaceWith(blockquote);
     }
   }
+
+  // Decorate the author as a cite element
+  const authorDiv = block.querySelector(":scope > div > div");
+  if (authorDiv) {
+    const cite = document.createElement("cite");
+    cite.innerHTML = `${authorDiv.innerHTML.trim()}`;
+    authorDiv.parentElement.replaceWith(cite);
+  }
+}
